ff602719462b35f38acb7844bb33f732758ffcc2
richardColes <125573778+richardColes@users.noreply.github.com>
2023-04-21 21:24:27 +0100

    Add files via upload

--- gazetteer/js/script.js
+++ gazetteer/js/script.js
@@ -32,16 +32,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       country: country.iso_a2,
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         country.area = result['data']['geonames']['0']['areaInSqKm'];
-//         country.capitalCity = result['data']['geonames']['0']['capital'];
-//         country.continent = result['data']['geonames']['0']['continentName'];
-//         country.currencyCode = result['data']['geonames']['0']['currencyCode'];
-//         country.languages = result['data']['geonames']['0']['languages'];
-//         country.population = result['data']['geonames']['0']['population'];
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -58,14 +53,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       code: country.iso_a2,
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         country.covid_total.push(result['data']['data']['latest_data']['confirmed'],result['data']['data']['latest_data']['deaths'],result['data']['data']['latest_data']['recovered']);
-//         country.covid_latest.push(result['data']['data']['timeline']['0']['new_confirmed'],result['data']['data']['timeline']['0']['new_deaths'],result['data']['data']['timeline']['0']['new_recovered']);
-//         country.covid_3MonthsAgo.push(result['data']['data']['timeline']['90']['confirmed'],result['data']['data']['timeline']['90']['deaths'],result['data']['data']['timeline']['90']['recovered']);
-//         country.covid_6MonthsAgo.push(result['data']['data']['timeline']['180']['confirmed'],result['data']['data']['timeline']['180']['deaths'],result['data']['data']['timeline']['180']['recovered']);
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -81,10 +73,12 @@ L.control.layers(baseMaps).addTo(map);
 //     url: "exchangeRates.php",
 //     type: 'GET',
 //     dataType: 'json',
-//     data: {},
+//     data: {
+//       ???
+//     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         country.exchangeRate = result['data']['rates'][country.currencyCode];
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -101,14 +95,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       country: country.iso_a2,
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         // Modal 4 - News Articles - ([Image] [Title] [URL] [Source] [Published])
-//         country.news_article1.push(result['data']['articles']['0']['urlToImage'], result['data']['articles']['0']['title'], result['data']['articles']['0']['url'], result['data']['articles']['0']['source']['name'], result['data']['articles']['0']['publishedAt']);
-//         country.news_article2.push(result['data']['articles']['1']['urlToImage'], result['data']['articles']['1']['title'], result['data']['articles']['1']['url'], result['data']['articles']['1']['source']['name'], result['data']['articles']['1']['publishedAt']);
-//         country.news_article3.push(result['data']['articles']['2']['urlToImage'], result['data']['articles']['2']['title'], result['data']['articles']['2']['url'], result['data']['articles']['2']['source']['name'], result['data']['articles']['2']['publishedAt']);
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -125,13 +116,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       lat: country.lat,
-//       lng: country.lng,
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         country.timezone = result['data']['timezoneId'];
-//         country.timeOffset = result['data']['dstOffset'];
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -148,17 +137,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       alpha3: country.iso_a3,
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         country.flag = result['data']['flag'];
-//         country.languages = result['data']['languages']['0']['name'];
-//         country.currencyCode = result['data']['currencies']['0']['code'];
-//         country.currencyName = result['data']['currencies']['0']['name'];
-//         country.currencySymbol = result['data']['currencies']['0']['symbol'];
-//         country.topLevelDomain = result['data']['topLevelDomain']['0'];
-//         country.callingCode = result['data']['callingCodes'];
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -175,20 +158,11 @@ L.control.layers(baseMaps).addTo(map);
 //     type: 'GET',
 //     dataType: 'json',
 //     data: {
-//       lat: country.lat,
-//       lng: country.lng $('#selGeonameId').val()
+//       ???
 //     },
 //     success: function(result) {
 //       if (result.status.name == "ok") {
-//         $('#txtToponymName').html(result['data'][0]['toponymName']);
-//         $('#txtName').html(result['data'][0]['name']);
-//         $('#txtLat').html(result['data'][0]['lat']);
-//         $('#txtLng').html(result['data'][0]['lng']);
-//         $('#txtGeonameId').html(result['data'][0]['geonameId']);
-//         $('#txtCountryCode').html(result['data'][0]['countryCode']);
-//         $('#txtCountryName').html(result['data'][0]['countryName']);
-//         $('#txtFcl').html(result['data'][0]['fcl']);
-//         $('#txtFcode').html(result['data'][0]['fcode']);
+//         ???
 //       }
 //     },
 //     error: function(jqXHR, textStatus, errorThrown) {
@@ -199,7 +173,76 @@ L.control.layers(baseMaps).addTo(map);
 //   });
 // };
 
+function countryData() {
+  var polygon = L.polygon([
+    [23.75975,-77.53466],
+    [23.71,-77.78],
+    [24.28615,-78.03405],
+    [24.57564,-78.40848],
+    [25.2103,-78.19087],
+    [25.17,-77.89],
+    [24.34,-77.54],
+    [23.75975,-77.53466],
+    [26.58,-77.82],
+    [26.42,-78.91],
+    [26.79,-78.98],
+    [26.87,-78.51],
+    [26.84,-77.85],
+    [26.58,-77.82],
+    [26.59,-77],
+    [25.87918,-77.17255],
+    [26.00735,-77.35641],
+    [26.53,-77.34],
+    [26.92516,-77.78802],
+    [27.04,-77.79],
+    [26.59,-77]
+  ]).addTo(map);
+}
 
+document.getElementById("news").addEventListener("click", countryData);
+
+// function countryBorder() {
+//   $.ajax({
+//     url: "",
+//     type: 'GET',
+//     dataType: 'json',
+//     data: {
+//       ???
+//     },
+//     success: function(result) {
+//       if (result.status.name == "ok") {
+//         var polygon = L.polygon([
+//           [23.75975,-77.53466],
+//           [23.71,-77.78],
+//           [24.28615,-78.03405],
+//           [24.57564,-78.40848],
+//           [25.2103,-78.19087],
+//           [25.17,-77.89],
+//           [24.34,-77.54],
+//           [23.75975,-77.53466],
+//           [26.58,-77.82],
+//           [26.42,-78.91],
+//           [26.79,-78.98],
+//           [26.87,-78.51],
+//           [26.84,-77.85],
+//           [26.58,-77.82],
+//           [26.59,-77],
+//           [25.87918,-77.17255],
+//           [26.00735,-77.35641],
+//           [26.53,-77.34],
+//           [26.92516,-77.78802],
+//           [27.04,-77.79],
+//           [26.59,-77]
+//         ]).addTo(map);
+//       }
+//     },
+//     error: function(jqXHR, textStatus, errorThrown) {
+//       console.log(JSON.stringify(jqXHR));
+//       console.log(JSON.stringify(textStatus));
+//       console.log(JSON.stringify(errorThrown));
+//     }
+//   });
+// };
 
 // // // load GeoJSON from an external file
 // // $.getJSON("countryBorders.geo",function(data) {
@@ -237,29 +280,6 @@ L.control.layers(baseMaps).addTo(map);
 
 // map.on('click', onMapClick);
 
-var polygon = L.polygon([
-  [23.75975,-77.53466],
-  [23.71,-77.78],
-  [24.28615,-78.03405],
-  [24.57564,-78.40848],
-  [25.2103,-78.19087],
-  [25.17,-77.89],
-  [24.34,-77.54],
-  [23.75975,-77.53466],
-  [26.58,-77.82],
-  [26.42,-78.91],
-  [26.79,-78.98],
-  [26.87,-78.51],
-  [26.84,-77.85],
-  [26.58,-77.82],
-  [26.59,-77],
-  [25.87918,-77.17255],
-  [26.00735,-77.35641],
-  [26.53,-77.34],
-  [26.92516,-77.78802],
-  [27.04,-77.79],
-  [26.59,-77]
-]).addTo(map);
 
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 // circle.bindPopup("I am a circle.");
