0a7416f512405f42189ffb3aaccbf0cf3f3d14d5
richardColes <125573778+richardColes@users.noreply.github.com>
2023-05-18 19:48:53 +0100

    Add files via upload

--- gazetteer/php/countryBorders.php
+++ gazetteer/php/countryBorders.php
@@ -1,4 +1,18 @@
 <?php
+  $url = 'countryBorders.geo.json';
+  $data = file_get_contents($url);
+  $decode = json_decode($data, true);
+
+  $output['status']['code'] = "200";
+  $output['status']['name'] = "ok";
+  $output['status']['description'] = "success";
+  $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+  $output['data'] = $decode;
+
+  echo json_encode($output);
+?>
+
+<!--
   ini_set('display_errors', 'On');
   error_reporting(E_ALL);
 
@@ -22,9 +36,9 @@
   $output['status']['description'] = "success";
   $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
 
-  $output['data'] = $result;
+  $output['data'] = $decode;
 
-//   header('Content-Type: application/json; charset=UTF-8');
+  header('Content-Type: application/json; charset=UTF-8');
 
   echo json_encode($output);
-?>
+-->
