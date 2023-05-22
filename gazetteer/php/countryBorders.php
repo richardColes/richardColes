7814312bf11cf45feed7e46199fcabf7a115df30
richardColes <125573778+richardColes@users.noreply.github.com>
2023-05-22 23:16:25 +0100

    Add files via upload

--- gazetteer/php/countryBorders.php
+++ gazetteer/php/countryBorders.php
@@ -1,4 +1,5 @@
 <?php
+  $executionStartTime = microtime(true);
   $url = 'countryBorders.geo.json';
   $data = file_get_contents($url);
   $decode = json_decode($data, true);
@@ -9,36 +10,7 @@
   $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
   $output['data'] = $decode;
 
-  echo json_encode($output);
-?>
-
-<!--
-  ini_set('display_errors', 'On');
-  error_reporting(E_ALL);
-
-  $executionStartTime = microtime(true);
-
-  $countryBorders = file_get_contents('countryBorders.geo.json', true);
-
-  $decode = json_decode($countryBorders, true);
-
-  $result;
-
-  foreach($decode['features'] as $country) {
-    if($country['properties']['iso_a2'] == $_REQUEST['code']) {
-        $result = $country['geometry'];
-        break;
-    }
-  }
-
-  $output['status']['code'] = "200";
-  $output['status']['name'] = "ok";
-  $output['status']['description'] = "success";
-  $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-
-  $output['data'] = $decode;
-
   header('Content-Type: application/json; charset=UTF-8');
 
   echo json_encode($output);
--->
+?>
